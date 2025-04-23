ebb01d3672c06b787e48f8787f4ad5b9b18c3add
Jimmy García <jimmyegc@gmail.com>
2025-04-22 21:39:29 -0600

    first commit

---
+++ src/i18n/utils.ts
@@ -0,0 +1,15 @@
+import { labels as ui } from './ui';
+
+const defaultLang = "es"
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split('/');
+  if (lang in ui) return lang as keyof typeof ui;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
